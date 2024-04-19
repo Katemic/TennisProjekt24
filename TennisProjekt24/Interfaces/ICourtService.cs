db35bc8f5f023b98f90c1f5584755d0de6858be8
Magnus Ansbjerg Selchou Hansen <mah012@edu.zealand.dk>
2024-04-19 10:17:12 +0200

    Interface

--- TennisProjekt24/Interfaces/ICourtService.cs
+++ TennisProjekt24/Interfaces/ICourtService.cs
@@ -1,6 +1,14 @@
-﻿namespace TennisProjekt24.Interfaces
+﻿using TennisProjekt24.Models;
+
+namespace TennisProjekt24.Interfaces
 {
     public interface ICourtService
     {
+        bool AddCourt();
+        bool DeleteCourt();
+        bool UpdateCourt();
+        Court GetCourt();
+        List<Court> GetAllCourts();
+        void ChangeAvailability();
     }
 }
