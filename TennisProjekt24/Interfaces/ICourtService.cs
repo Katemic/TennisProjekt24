54c2b0e40b00345dd1dc0e76104f2d0bf4e55fb7
Magnus Ansbjerg Selchou Hansen <mah012@edu.zealand.dk>
2024-04-19 10:42:20 +0200

    Interface fix og package add

--- TennisProjekt24/Interfaces/ICourtService.cs
+++ TennisProjekt24/Interfaces/ICourtService.cs
@@ -4,11 +4,11 @@ namespace TennisProjekt24.Interfaces
 {
     public interface ICourtService
     {
-        bool AddCourt();
-        bool DeleteCourt();
-        bool UpdateCourt();
-        Court GetCourt();
+        bool AddCourt(Court court);
+        bool DeleteCourt(int CourtId);
+        bool UpdateCourt(int CourtId, Court court);
+        Court GetCourt(int CourtId);
         List<Court> GetAllCourts();
-        void ChangeAvailability();
+        void ChangeAvailability(int CourtId);
     }
 }
--- TennisProjekt24/Services/CourtService.cs
+++ TennisProjekt24/Services/CourtService.cs
@@ -1,6 +1,38 @@
-﻿namespace TennisProjekt24.Services
+﻿using TennisProjekt24.Interfaces;
+using TennisProjekt24.Models;
+
+namespace TennisProjekt24.Services
 {
-    public class CourtService
+    public class CourtService : ICourtService
     {
+        public bool AddCourt(Court court)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeAvailability(int CourtId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteCourt(int CourtId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Court> GetAllCourts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Court GetCourt(int CourtId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateCourt(int CourtId, Court court)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
