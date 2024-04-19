458e35b0971a87e905c8b0292cd1440edfe631f3
Magnus Ansbjerg Selchou Hansen <mah012@edu.zealand.dk>
2024-04-19 17:25:34 +0200

    courtService

--- TennisProjekt24/Interfaces/ICourtService.cs
+++ TennisProjekt24/Interfaces/ICourtService.cs
@@ -5,10 +5,10 @@ namespace TennisProjekt24.Interfaces
     public interface ICourtService
     {
         bool AddCourt(Court court);
-        bool DeleteCourt(int CourtId);
-        bool UpdateCourt(int CourtId, Court court);
-        Court GetCourt(int CourtId);
+        bool DeleteCourt(int courtId);
+        bool UpdateCourt(int courtId, Court court);
+        Court GetCourt(int courtId);
         List<Court> GetAllCourts();
-        void ChangeAvailability(int CourtId);
+        void ChangeAvailability(int courtId);
     }
 }
--- TennisProjekt24/Services/CourtService.cs
+++ TennisProjekt24/Services/CourtService.cs
@@ -1,38 +1,214 @@
-﻿using TennisProjekt24.Interfaces;
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using TennisProjekt24.Interfaces;
 using TennisProjekt24.Models;
 
 namespace TennisProjekt24.Services
 {
-    public class CourtService : ICourtService
+    public class CourtService : Connection, ICourtService
     {
+        private string _getAllString = "SELECT * FROM Courts";
+        private string _getByIdSql = "SELECT * FROM Courts WHERE CourtId=@CourtId";
+        private string _insertSql = "INSERT INTO Courts VALUES(@Outdoor, @CourtNumber, @CourtType, @Availability)";
+        private string _deleteSql = "DELETE FROM Courts WHERE CourtId=@CourtId";
+        private string _updateSql = "UPDATE Courts SET Outdoor=@Outdoor, CourtNumber=@CourtNumber, CourtType=@CourtType, Availability=@Availability WHERE CourtId=@CourtId";
+        private string _availabilitySql = "UPDATE Courts SET Availability=@Availability WHERE CourtId=@CourtId";
+
         public bool AddCourt(Court court)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    SqlCommand command = new SqlCommand(_insertSql, connection);
+                    command.Parameters.AddWithValue("@Outdoor", court.Outdoor);
+                    command.Parameters.AddWithValue("@CourtNumber", court.CourtNumber);
+                    command.Parameters.AddWithValue("@CourtType", court.CourtType);      //needs correcting
+                    command.Parameters.AddWithValue("@Availability", court.Availability);
+                    command.Connection.Open();
+                    int noOfRows = command.ExecuteNonQuery();
+                    return noOfRows == 1;
+                }
+                catch (SqlException sqlEx)
+                {
+                    Console.WriteLine("Database error: " + sqlEx.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("General fejl: " + ex.Message);
+                }
+                finally
+                {
+
+                }
+            }
+            return false;
         }
 
-        public void ChangeAvailability(int CourtId)
+        public void ChangeAvailability(int courtId)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    SqlCommand command = new SqlCommand(_availabilitySql, connection);
+                    command.Parameters.AddWithValue("@CourtId", courtId);
+                    if (GetCourt(courtId).Availability)
+                        command.Parameters.AddWithValue("@Availability", true);
+                    else
+                        command.Parameters.AddWithValue("@Availability", false);
+                    command.Connection.Open();
+                    int noOfRows = command.ExecuteNonQuery();
+                }
+                catch (SqlException sqlEx)
+                {
+                    Console.WriteLine("Database error: " + sqlEx.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("General fejl: " + ex.Message);
+                }
+                finally
+                {
+
+                }
+            }
         }
 
-        public bool DeleteCourt(int CourtId)
+        public bool DeleteCourt(int courtId)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    SqlCommand deleteCommand = new SqlCommand(_deleteSql, connection);
+                    deleteCommand.Parameters.AddWithValue("@CourtId", courtId);
+                    deleteCommand.Connection.Open();
+                    deleteCommand.ExecuteNonQuery();
+                    deleteCommand.Connection.Open();
+                    int noOfRows = deleteCommand.ExecuteNonQuery();
+                    return noOfRows == 1;
+                }
+                catch (SqlException sqlEx)
+                {
+                    Console.WriteLine("Database error: " + sqlEx.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("General fejl: " + ex.Message);
+                }
+                finally
+                {
+
+                }
+            }
+            return false;
         }
 
         public List<Court> GetAllCourts()
         {
-            throw new NotImplementedException();
+            List<Court> courts = new List<Court>();
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    try
+            //    {
+            //        SqlCommand command = new SqlCommand(_getAllString, connection);
+            //        command.Connection.Open();
+            //        SqlDataReader reader = command.ExecuteReader();
+            //        while (reader.Read())
+            //        {
+            //            int courtId = reader.GetInt32("CourtId");
+            //            bool outdoor = reader.GetBoolean("Outdoor");
+            //            double courtNumber = reader.GetDouble("CourtNumber");
+            //            int courtType = reader.GetInt32("CourtType");
+            //            int availability = reader.GetInt32("Availability");
+            //            Court court = new Court(courtId, outdoor, courtNumber, courtType, availability);
+            //            courts.Add(court);
+            //        }
+            //        reader.Close();
+            //    }
+            //    catch (SqlException sqlEx)
+            //    {
+            //        Console.WriteLine("Database error: " + sqlEx.Message);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine("General fejl: " + ex.Message);
+            //    }
+            //    finally
+            //    {
+
+            //    }
+            //}
+            return courts;
         }
 
-        public Court GetCourt(int CourtId)
+        public Court GetCourt(int courtId)
         {
-            throw new NotImplementedException();
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    try
+            //    {
+            //        SqlCommand command = new SqlCommand(_getByIdSql, connection);
+            //        command.Parameters.AddWithValue("@CourtId", courtId);
+            //        command.Connection.Open();
+            //        SqlDataReader reader = command.ExecuteReader();
+            //        if (reader.Read())
+            //        {
+            //            bool outdoor = reader.GetBoolean("Outdoor");
+            //            double courtNumber = reader.GetDouble("CourtNumber");
+            //            int courtType = reader.GetInt32("CourtType");
+            //            int availability = reader.GetInt32("Availability");
+            //            Court court = new Court(courtId, outdoor, courtNumber, courtType, availability);
+            //            return court;
+            //        }
+            //    }
+            //    catch (SqlException sqlEx)
+            //    {
+            //        Console.WriteLine("Database error: " + sqlEx.Message);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine("General fejl: " + ex.Message);
+            //    }
+            //    finally
+            //    {
+
+            //    }
+            //}
+            return null;
         }
 
-        public bool UpdateCourt(int CourtId, Court court)
+        public bool UpdateCourt(int courtId, Court court)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    SqlCommand command = new SqlCommand(_updateSql, connection);
+                    command.Parameters.AddWithValue("@CourtId", courtId);
+                    command.Parameters.AddWithValue("@Outdoor", court.Outdoor);
+                    command.Parameters.AddWithValue("@CourtNumber", court.CourtNumber);
+                    command.Parameters.AddWithValue("@CourtType", court.CourtType);
+                    command.Parameters.AddWithValue("@Availability", court.Availability);
+                    command.Connection.Open();
+                    int noOfRows = command.ExecuteNonQuery();
+                    return noOfRows == 1;
+                }
+                catch (SqlException sqlEx)
+                {
+                    Console.WriteLine("Database error: " + sqlEx.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("General fejl: " + ex.Message);
+                }
+                finally
+                {
+
+                }
+            }
+            return false;
         }
     }
 }
