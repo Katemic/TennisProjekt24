ed04158ec6aa76b0a0a6b580d824c90bdc50d3f9
Gustav Vang Klinker <guk001@edu.zealand.dk>
2024-04-19 09:55:35 +0200

    IPractice done;

--- TennisProjekt24/Interfaces/IPracticeService.cs
+++ TennisProjekt24/Interfaces/IPracticeService.cs
@@ -1,6 +1,16 @@
-﻿namespace TennisProjekt24.Interfaces
+﻿using TennisProjekt24.Models;
+
+namespace TennisProjekt24.Interfaces
 {
-    public class IPracticeService
+    public interface IPracticeService
     {
+        Practice GetPractice(int id);
+        bool AddPractice(Practice practice);
+        bool DeletePractice(int id);
+        bool UpdatePractice(Practice practice, int id);
+        List<Practice> GetAllPractices();
+        List<Practice> GetPracticeByType();
+
+
     }
 }
