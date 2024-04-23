0f012684f7525762cebeb3f47ac352966c0d6974
Katrine <katrinemichala@gmail.com>
2024-04-23 13:24:06 +0200

    validering

--- TennisProjekt24/Models/Member.cs
+++ TennisProjekt24/Models/Member.cs
@@ -13,25 +13,32 @@ namespace TennisProjekt24.Models
         public int MemberId { get; set; }
 
         [Required(ErrorMessage = "Udfyld brugernavn")]
+        [StringLength(30,ErrorMessage ="Brugernavn kan ikke være længere end 30 karakterer")] 
         public string Username { get; set; }
 
         [Required(ErrorMessage ="Udfyld password")]
+        [StringLength(30, ErrorMessage = "Password kan ikke være længere end 30 karakterer")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Indtast navn")]
+        [StringLength(50, ErrorMessage = "Navn kan ikke være længere end 50 karakterer")]
         public string Name { get; set; }
 
         [Required(ErrorMessage ="Indtast email")]
-        [EmailAddress(ErrorMessage ="Indtast gyldig email")]
+        [EmailAddress(ErrorMessage ="Indtast gyldig email")] //muligvis regulært udtryk i stedet 
+        [StringLength(50, ErrorMessage = "Email kan ikke være længere end 50 karakterer")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Indtast telefonnummer")]
+        [StringLength(11, ErrorMessage = "Telefon nummer skal være mellem 8 og 11 karakterer", MinimumLength = 8)]
         public string PhoneNumber { get; set; }
 
         [Required(ErrorMessage = "Indtast adresse")]
+        [StringLength(50, ErrorMessage = "Adressen kan ikke være længere end 50 karakterer")]
         public string Address { get; set; }
 
         [Required(ErrorMessage = "Indtast postnummer")]
+        [StringLength(4, ErrorMessage = "Post nummer skal være 4 tegn",MinimumLength = 4)]
         public string PostCode { get; set; }
 
         [Required(ErrorMessage = "vælg medlemstype")]
