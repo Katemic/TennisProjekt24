656536515ba78e00c38f39dabd459b398d0e7ac9
Gustav Vang Klinker <guk001@edu.zealand.dk>
2024-04-23 13:55:39 +0200

    It work

--- TennisProjekt24/Pages/Practices/UpdatePractice.cshtml.cs
+++ TennisProjekt24/Pages/Practices/UpdatePractice.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TennisProjekt24.Interfaces;
+using TennisProjekt24.Models;
 
 namespace TennisProjekt24.Pages.Practices
 {
     public class UpdatePracticeModel : PageModel
     {
-        public void OnGet()
+        private IPracticeService _service;
+        [BindProperty]
+        public Practice Practice { get; set; }
+        [BindProperty]
+        public PracticeTypeEnum Type { get; set; }
+        public void OnGet(int id)
         {
+            Practice = _service.GetPractice(id);
+        }
+
+        public UpdatePracticeModel(IPracticeService serv)
+        {
+            _service = serv;
+        }
+
+
+        public IActionResult OnPostUpdate()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if(_service.UpdatePractice(Practice, Practice.PracticeId))
+                return RedirectToPage("Index");
+            else
+                return Page();
         }
     }
 }
