3134d4ae686bd765cf366c8aef28b2eb5d341793
Gustav Vang Klinker <guk001@edu.zealand.dk>
2024-04-25 09:15:47 +0200

    Merge med Development;

--- TennisProjekt24/Models/Practice.cs
+++ TennisProjekt24/Models/Practice.cs
@@ -1,4 +1,6 @@
-﻿namespace TennisProjekt24.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TennisProjekt24.Models
 {
     public enum PracticeTypeEnum
     {
@@ -7,10 +9,16 @@
     public class Practice
     {
         public int PracticeId {  get; set; }
+        [Required(ErrorMessage = "Date must not be empty")]
         public DateTime StartDate { get; set; }
+        [Required(ErrorMessage = "Title must not be empty")]
+        [StringLength(50)]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Number of training must not be 0")]
         public int NoOfTrainings { get; set; }
+        [Required(ErrorMessage = "Max attendees must not be 0")]
         public int MaxNoOfAttendees { get; set; }
+        [Required(ErrorMessage ="ID must not be empty")]
         public int InstructorId { get; set; }
         public PracticeTypeEnum Type { get; set; }
 
