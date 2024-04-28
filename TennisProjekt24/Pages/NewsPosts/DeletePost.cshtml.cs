c887e8abf36eb00f0923eddc4294b7110071864b
Katrine <katrinemichala@gmail.com>
2024-04-28 18:21:26 +0200

    pages

--- TennisProjekt24/Pages/NewsPosts/DeletePost.cshtml.cs
+++ TennisProjekt24/Pages/NewsPosts/DeletePost.cshtml.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TennisProjekt24.Interfaces;
+using TennisProjekt24.Models;
 
 namespace TennisProjekt24.Pages.NewsPosts
 {
     public class DeletePostModel : PageModel
     {
-        public void OnGet()
+        private INewsPostService _newsPostService;
+
+        public NewsPost PostToDelete { get; set; }
+
+        public DeletePostModel(INewsPostService newsPostService)
         {
+            _newsPostService = newsPostService;
         }
+
+
+        public void OnGet(int id)
+        {
+            PostToDelete = _newsPostService.GetPost(id);
+        }
+
+
+        public IActionResult OnPost(int id) 
+        {
+            _newsPostService.DeletePost(id);
+            return RedirectToPage("index");
+        
+        }
+
     }
 }
--- TennisProjekt24/Pages/NewsPosts/UpdatePost.cshtml.cs
+++ TennisProjekt24/Pages/NewsPosts/UpdatePost.cshtml.cs
@@ -1,12 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TennisProjekt24.Interfaces;
+using TennisProjekt24.Models;
 
 namespace TennisProjekt24.Pages.NewsPosts
 {
     public class UpdatePostModel : PageModel
     {
-        public void OnGet()
+
+        private INewsPostService _newsPostService;
+
+
+        [BindProperty]
+        public NewsPost UpdatedPost { get; set; }
+
+        public UpdatePostModel(INewsPostService newsPostService)
         {
+             _newsPostService = newsPostService;
+        }
+
+        public void OnGet(int id)
+        {
+            UpdatedPost = _newsPostService.GetPost(id);
+        }
+
+        public IActionResult OnPost() 
+        {
+            
+            if (!ModelState.IsValid) 
+            {
+                return Page();
+            }
+
+            _newsPostService.UpdatePost(UpdatedPost, UpdatedPost.NewsPostId);
+            return RedirectToPage("Index");
         }
     }
 }
