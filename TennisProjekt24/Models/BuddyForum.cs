15c4cce55e01accb0662382ea4ad88a829ed2d02
Magnus-Hansen <54136904+Magnus-Hansen@users.noreply.github.com>
2024-04-29 13:50:40 +0200

    model

---
+++ TennisProjekt24/Models/BuddyForum.cs
@@ -0,0 +1,24 @@
+﻿namespace TennisProjekt24.Models
+{
+    public class BuddyForum
+    {
+        public int PostId { get; set; }
+        public DateTime DateTime { get; set; }
+        public int MemberId { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        public BuddyForum()
+        {
+            
+        }
+        public BuddyForum(int postId, DateTime dateTime, int memberId, string title, string text)
+        {
+            PostId = postId;
+            DateTime = dateTime;
+            MemberId = memberId;
+            Title = title;
+            Text = text;
+        }
+    }
+}
