eab2b701f15be02ee98f6b9b4e19f61329d4002e
Gustav Vang Klinker <guk001@edu.zealand.dk>
2024-05-15 09:54:55 +0200

    Tried more secutiry, bit it bbroke he site instead. Looking into others code

--- TennisProjekt24/Pages/Practices/Index.cshtml.cs
+++ TennisProjekt24/Pages/Practices/Index.cshtml.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
 using TennisProjekt24.Interfaces;
 using TennisProjekt24.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace TennisProjekt24.Pages.Practices
 {
@@ -9,13 +12,21 @@ namespace TennisProjekt24.Pages.Practices
     {
         public List<Practice> Practices { get; set; }
         IPracticeService practiceService { get; set; }
+        IMemberService memberService { get; set; }
+        public Member User {  get; set; }
         public void OnGet()
         {
+            User = memberService.GetMember((int)HttpContextAccessor.HttpContext.Session.GetInt32("MemberId"));
             Practices = practiceService.GetAllPractices(null);
+            foreach (var practice in Practices)
+            {
+                practice.Members.AddRange(memberService.GetAllMembers(practice.PracticeId));
+            }
         }
-        public IndexModel(IPracticeService prac)
+        public IndexModel(IPracticeService prac, IMemberService memberService)
         {
             practiceService = prac;
+            this.memberService = memberService;
         }
     }
 }
