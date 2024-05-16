93c0e38b525a9d6de573fc56c5ae6ff040ab03c2
Gustav Vang Klinker <guk001@edu.zealand.dk>
2024-05-16 12:19:19 +0200

    Show more

--- TennisProjekt24/Pages/Instructors/AddInstructor.cshtml.cs
+++ TennisProjekt24/Pages/Instructors/AddInstructor.cshtml.cs
@@ -36,10 +36,10 @@ namespace TennisProjekt24.Pages.Instructors
 
         public IActionResult OnPost()
         {
-            //if (!ModelState.IsValid)
-            //{
-            //    return Page();
-            //}
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
             if (Photo != null)
             {
                 if (Instructor.Image != null)
