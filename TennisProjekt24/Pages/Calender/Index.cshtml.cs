995cbb9da988e54768814e047834c003920579bd
Magnus-Hansen <54136904+Magnus-Hansen@users.noreply.github.com>
2024-05-16 22:09:13 +0200

    temp

--- TennisProjekt24/Pages/Calender/Index.cshtml.cs
+++ TennisProjekt24/Pages/Calender/Index.cshtml.cs
@@ -5,8 +5,9 @@ namespace TennisProjekt24.Pages.Calender
 {
     public class IndexModel : PageModel
     {
-        public int CurrentYear { get; set; }
-        public int CurrentMonth { get; set; }
+        public int CurrentYear { get; set; } = DateTime.Now.Year;
+        public int CurrentMonth { get; set; } = DateTime.Now.Month;
+        public DateTime CurrentDay { get; set; }
         public void OnGet()
         {
         }
