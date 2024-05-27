18b7959082a941581759bf6958800dfb3aee55b2
Katrine <kah003@edu.zealand.dk>
2024-05-27 12:33:44 +0200

    b

--- TennisProjekt24/Models/Participant.cs
+++ TennisProjekt24/Models/Participant.cs
@@ -8,7 +8,7 @@ namespace TennisProjekt24.Models
         public Event Event { get; set; }
         public Member Member { get; set; }
         [Required(ErrorMessage = "Vælg antal deltagere")]
-        [Range(1, 5, ErrorMessage = "Antal skal være mellem 1 og 5")]
+        [Range(1, 10, ErrorMessage = "Antal skal være mellem 1 og 10")]
         public int NoOfParticipants { get; set; }
         public string Note { get; set; }
 
